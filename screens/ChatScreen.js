92c8be4b572cb2aae55b8ec1598ae3e0e2c5116e
moemad <moemad504@gmail.com>
2025-10-09 14:02:22 +0300

    Add Inox screen with the logic and ui of the chat screen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,10 +1,50 @@
-import React from "react";
-import { View, Text } from "react-native";
+import axios from "axios";
+import React, { useState } from "react";
+import { View, Text, FlatList } from "react-native";
+import { Avatar, Card, TextInput } from "react-native-paper";
 
-export const ChatScreen = () => {
+export const ChatScreen = ({ route }) => {
+    const { name, age, image } = route.params;
+    const [message, setMessage] = useState('')
+    const [data,setData] = useState([])
+
+    console.log(route.params);
+    
+//     const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9tZGExQGdtYWlsLmNvbSIsImlkIjoiaFo4ZkJxQzVGMndKMXFON2VJdGoiLCJyb2xlIjoidXNlciIsIm5hbWUiOiJzbGFoYSAiLCJ1c2VySW1hZ2VVcmwiOm51bGwsImlhdCI6MTc1OTk1NDk1MSwiZXhwIjoxNzYwMDQxMzUxfQ.Pnb3QNnYv8y3L-Wgiiwvar52--2zZuvzVBTM5IvwKfk'
+//     const url = 'http://localhost:5000/api/chats'
+
+// const sendMessage = async () => {
+//   try {
+//     const res = await axios.get(url, {
+//         headers: { Authorization: token },
+//     });
+//     setData(res.data);
+//     console.log(res.data);
+//   } catch (error) {
+//     console.log(error.response?.data || error.message);
+//   }
+// };
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={{ paddingRight: 10 ,paddingLeft: 10 ,display:'flex',gap:10, flexDirection:'column',justifyContent:'space-between', height:'100%'}}>
+    <View style={{ padding: 20 ,display:'flex',gap:10, flexDirection:'row', alignItems:'center'}}>
+      <Avatar.Image size={50} source={require("../assets/IMG-20210822-WA0009.jpg")} />
+      <Text style={{ fontSize: 24, fontWeight: "" }}>{name}</Text>
     </View>
+    <View>
+        <TextInput
+            mode="outlined"
+            label="Write Your Message"
+            placeholder="Type something"
+            right={<TextInput.Icon icon="send" onPress={()=> {sendMessage()}}/>}
+            activeUnderlineColor="#2ca0e8ff"
+            underlineColor="#2ca0e8ff"
+            value={message}
+            onChangeText={(text)=> {
+                setMessage(text)
+            }}
+        />
+    </View>
+    </View>  
   );
 };
---
+++ screens/InobxScreen.js
@@ -0,0 +1,155 @@
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { View, Text, Image, TouchableOpacity, FlatList } from "react-native";
+import { Card, IconButton, TextInput } from "react-native-paper";
+
+export const InobxScreen = () => {
+  const navigation = useNavigation();
+  const [data, setData] = useState([]);
+  const [myData, setMyData] = useState([]);
+  const [frindsData, setFrindsData] = useState([]);
+  const token =
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vZW1hZDUwNkBnbWFpbC5jb20iLCJpZCI6IjNvWjlNZWczT29tWnhoeDMxY3BGIiwicm9sZSI6InVzZXIiLCJuYW1lIjoibWFtZG91aCIsInVzZXJJbWFnZVVybCI6bnVsbCwiaWF0IjoxNzU5OTcwNzc2LCJleHAiOjE3NjAwNTcxNzZ9.cYvadkeBS4wk6KF8aAYOLuRFk5eu76Ym_ZquBcynoWk";
+  const url = "http://localhost:5000/api/chats";
+  const getMyId = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/api/users/me", {
+        headers: { Authorization: token },
+      });
+      setMyData(res.data);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //  get all chats
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(url, {
+        headers: { Authorization: token },
+      });
+      setData(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error.response?.data || error.message);
+    }
+  };
+  // get frinds ID
+  const getMyFrindId = (chat, myId) => {
+    if (!Array.isArray(chat)) return [];
+    const ids = chat.flatMap((chat) =>
+      chat.members.filter((memberId) => memberId !== myId)
+    );
+    return Array.from(new Set(ids));
+  };
+  // console.log(frindIds);
+
+  // get Frinds Data
+  const getFrindsData = async () => {
+    const frindIds = getMyFrindId(data, myData.id);
+    if (frindIds.length === 0) return;
+    try {
+      const res = await Promise.all(
+        frindIds.map((id) =>
+          axios.get(`http://localhost:5000/api/users/${id}`, {
+            headers: { Authorization: token },
+          })
+        )
+      );
+      const data = res.map((r) => r.data.user);
+      setFrindsData(data);
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // merge data
+  const mergeData = data.map((chat) => {
+
+    const frindId = chat.members.find((id) => id !== myData.id);
+    const friend  = frindsData.find((f) => f.id === frindId);
+
+    return {
+      ...chat,
+      friend: friend || {},
+    };
+  });
+  useEffect(() => {
+    getMyId();
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (data.length > 0 && myData.id) {
+      getFrindsData();
+    }
+  }, [data, myData]);
+console.log(mergeData);
+
+  return (
+    <View style={{ padding: 20 }}>
+      <TextInput
+        label="Search."
+        left={<TextInput.Icon icon="magnify" />}
+        activeOutlineColor="#2ca0e8ff"
+        underlineColor="#2ca0e8ff"
+      />
+
+        <View style={{ height: "90%" }}>
+          <FlatList
+            data={mergeData}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                onPress={() =>
+                  navigation.navigate("ChatScreen", {
+                    chatId:item.id,
+                    id: item.friend.id,
+                    name: item.friend.name,
+                    image:
+                      item.friend.userImageUrl ||
+                      require("../assets/IMG-20210822-WA0009.jpg"),
+                  })
+                }
+              >
+                <Card.Title
+                  title={item.friend?.name || "Unknown"}
+                  subtitle={item.lastMessage || ""}
+                  left={(props) => (
+                    <Image
+                      source={
+                        item.friend?.userImageUrl
+                          ? { uri: item.friend.userImageUrl }
+                          : require("../assets/IMG-20210822-WA0009.jpg")
+                      }
+                      style={{ width: 40, height: 40, borderRadius: 20 }}
+                    />
+                  )}
+                  right={(props) => (
+                    <View style={{ display: "flex", alignItems: "center" }}>
+                      <Text>
+                        {item.lastMessageAt
+                          ? new Date(item.lastMessageAt).toLocaleTimeString(
+                              [],
+                              {
+                                hour: "2-digit",
+                                minute: "2-digit",
+                              }
+                            )
+                          : ""}
+                      </Text>
+                    </View>
+                  )}
+                  titleStyle={{ fontWeight: "bold", fontSize: 16 }}
+                />
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+
+    </View>
+  );
+};
