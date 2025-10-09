9a80bc8cbe5e3370566488d4b4d8232fb985ef17
moemad <moemad504@gmail.com>
2025-10-09 14:04:21 +0300

    Add Inox screen with the logic and ui of the chat screen

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
- import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
- import { NavigationContainer } from '@react-navigation/native';
- import { StyleSheet, Text, View } from 'react-native';
- import AppNavigator from './navigation/AppNavigator';
- import LoginScreen from './screens/LoginScreen';
-
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AppNavigator from "./navigation/AppNavigator";
+import LoginScreen from "./screens/LoginScreen";
+import { StyleSheet, Text, View } from "react-native";
 export default function App() {
-  return (<>
-  
-    {/* <NavigationContainer>
-    <AppNavigator/> */}
-    {/* </NavigationContainer> */}
-    <LoginScreen/>
-    </>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Main" component={AppNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
